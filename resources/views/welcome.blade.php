6d3439f32085a7568a3952a16383dd6dab46d586
Your Name <you@example.com>
2024-08-25 23:20:25 -0300

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    @vite('resources/css/app.css')
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-        body{
-            background-color: red;
-        }
-    </style>
-    <title>Document</title>
+  <meta charset="utf-8">
+  @vite('resources/css/app.css')
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
 </head>
-<body>
-    <h1>testando o deploy</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos id, quos quod rerum, hic repudiandae, iusto incidunt obcaecati accusamus magni sint adipisci corrupti enim eaque eligendi saepe quibusdam quasi corporis.
-    Molestias aspernatur asperiores, repellendus iure magni, maxime totam tempore laudantium eos voluptatem corrupti provident odit? Et ea quisquam ipsum impedit sapiente deleniti rerum fugiat voluptas, dolor, enim recusandae, veniam assumenda!
-    Atque iste iusto aut necessitatibus beatae minus rerum eos distinctio molestiae corporis, doloremque, ipsam mollitia dolor, odio ab minima consectetur? Voluptatibus ipsa fugiat omnis modi error ratione rem repudiandae deserunt.
-    Natus deserunt est maxime illo amet necessitatibus odit error exercitationem, culpa molestiae reiciendis officiis et impedit commodi dolorem aut eum tenetur quasi cumque! Dicta qui neque veritatis, autem ab reprehenderit.
-    Possimus voluptas doloremque incidunt hic eaque obcaecati, voluptatum id officiis excepturi, quam vitae magni nulla voluptate cupiditate error sed quis sint. Doloremque fuga voluptas laudantium, adipisci odio ut vitae alias!
-    Impedit, dolorem eligendi. Dicta blanditiis eaque nisi illo non assumenda illum sint tenetur excepturi quidem atque quis repellat temporibus voluptatibus ratione enim reprehenderit, cum ad odio itaque quod libero voluptatem.
-    Maiores rerum, distinctio ab aliquam, eos consequatur doloribus ex perspiciatis ad voluptate vero commodi, inventore quidem quisquam blanditiis sed porro quo dolores! Iure sunt doloribus adipisci amet, officiis aliquid atque!
-    Laborum eius architecto itaque labore eaque quia officia accusamus culpa quae voluptas atque incidunt amet ullam, obcaecati accusantium illo repellendus aut perferendis animi qui neque voluptatem quibusdam. Qui, voluptatibus et?</p>
-    
+<body class="bg-red-600">
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </body>
 </html>
+   
+   
