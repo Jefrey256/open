19f80cb07e970e8b4fe577d84681b7edb63ee47c
Your Name <you@example.com>
2024-08-25 23:00:50 -0300

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    @vite('resources/css/app.css')
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body{
+            background-color: red;
+        }
+    </style>
     <title>Document</title>
 </head>
 <body>
