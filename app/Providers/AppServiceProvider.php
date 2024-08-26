637f72fefe1b4eba98d6aca8e7ec1ee37bc02bff
Your Name <you@example.com>
2024-08-26 01:23:10 -0300

    up

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     
-        if (env('APP_ENV') === 'production') {
+        if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
     }
